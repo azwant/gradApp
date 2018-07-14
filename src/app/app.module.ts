1adfcd666d086e9afb7f4421e6b6377dbd2acce8
Azwan Ali <itsazwan@gmail.com>
2018-07-15 01:03:36 +0800

    Database dah dapat call tapi data dia bercantum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyC467CRdoKEOaOyv0m7xaC2fuXNE4H7LKw",
+  authDomain: "gradappdb.firebaseapp.com",
+  databaseURL: "https://gradappdb.firebaseio.com",
+  projectId: "gradappdb",
+  storageBucket: "gradappdb.appspot.com",
+  messagingSenderId: "728716124964"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(config),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  items;
+  constructor(public navCtrl: NavController,
+  public afd: AngularFireDatabase) {
+    this.getDataFromFirebase()
   }
 
+
+  getDataFromFirebase(){
+    this.afd.list('/Juju/').valueChanges().subscribe(
+      data => {
+        console.log(JSON.stringify(data))
+        this.items = data;
+      }
+    )
+  }
 }
